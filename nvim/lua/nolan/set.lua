f79ea17aca97f8a296c3d2bcf5e9e17582a62d18
nolanwinsman <nolanwinsman@gmail.com>
2025-02-19 17:34:48 -0800

    sets created

---
+++ nvim/lua/nolan/set.lua
@@ -0,0 +1,27 @@
+local o = vim.opt
+
+o.nu = true
+o.relativenumber = true
+
+-- smarter indenting
+o.tabestop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+
+o.wrap = false
+
+o.hlsearch = false
+o.incsearch = false
+
+o.termguicolors = true
+
+-- always a buffer of 8 lines when scrolling up or down
+o.scrolloff = 8
+
+o.updatetime = 50
+
+o.colorcolumn = "80"
+
+-- o.g.mapleader = " "
